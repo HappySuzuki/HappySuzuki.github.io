f3e00c78cf5ac5d708c4bf645d04138857f132d4
HappySuzuki <kaisuzuki915@gmail.com>
2021-05-17 07:27:37 -0700

    free

---
+++ app/layouts/navbar/nav-data.js
@@ -0,0 +1,24 @@
+import config from '~/config.json';
+
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  {
+    label: 'Articles',
+    pathname: '/articles',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+
+];
